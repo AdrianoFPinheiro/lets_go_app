3a98f2fb285964366e5e3b066d5101d28120b149
2Mundos <adrianofpinheiro@outlook.com>
2019-11-11 17:10:57 -0300

    Grid, List, ProdutosActivity, Categorias,

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
       title: 'Loja Virtual Flutter',
       theme: ThemeData(
         primarySwatch: Colors.blue,
-        primaryColor: Color.fromARGB(255, 4, 125, 141),
+        primaryColor: Color.fromARGB(255, 0, 0, 255),
       ),
       debugShowCheckedModeBanner: false,
       home: HomeScreen(),
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,8 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:loja_virtual/tabs/ProductsTab.dart';
+import 'package:loja_virtual/tabs/home_tab.dart';
+import 'package:loja_virtual/widgets/custom_drawer.dart';
 
 class HomeScreen extends StatelessWidget {
+
+  final _pageController = PageController();
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return PageView(
+      controller: _pageController,
+      physics: NeverScrollableScrollPhysics(),
+      children: <Widget>[
+        Scaffold(
+          body: HomeTab(),
+          drawer: CustomDrawer(_pageController),
+        ),
+        Scaffold(
+          appBar: AppBar(
+            title: Text("Produtos"),
+            centerTitle: true,
+          ),
+          drawer: CustomDrawer(_pageController),
+          body: ProductsTab(),
+        ),
+        Container(color: Colors.green,),
+        Container(color: Colors.red,),
+      ],
+    );
   }
 }
