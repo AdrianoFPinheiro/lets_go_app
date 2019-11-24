00d96098177d9b2417eefa6778a80ce8508f415e
Adriano F. Pinheiro <adrianofpinheiro@outlook.com>
2019-11-24 20:36:06 -0300

    Configuração FirebaseAuth.

---
+++ lib/model/user_model.dart
@@ -0,0 +1,54 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:scoped_model/scoped_model.dart';
+
+class UserModel extends Model {
+  FirebaseAuth _auth = FirebaseAuth.instance;
+
+  FirebaseUser firebaseUser;
+
+  bool isLoading = false;
+
+  Map<String, dynamic> userData = Map();
+
+  void signUp(
+      {@required Map<String, dynamic> userData,
+      @required String pass,
+      @required VoidCallback onSuccess,
+      @required VoidCallback onFail}) {
+    isLoading = true;
+    notifyListeners();
+
+    _auth
+        .createUserWithEmailAndPassword(
+            email: userData["email"], password: pass)
+        .then((auth) async {
+      firebaseUser = auth.user;
+
+      await _saveUserData(userData);
+
+      onSuccess();
+      isLoading = false;
+      notifyListeners();
+    }).catchError((e) {
+      onFail();
+      isLoading = false;
+      notifyListeners();
+    });
+  }
+
+  void signIn() {}
+
+  void recoverPass() {}
+
+  bool isLoggedIn() {}
+
+  Future<Null> _saveUserData(Map<String, dynamic> userData) async {
+    this.userData = userData;
+    await Firestore.instance
+        .collection("users")
+        .document(firebaseUser.uid)
+        .setData(userData);
+  }
+}
--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:loja_virtual/model/user_model.dart';
 import 'package:loja_virtual/screens/signup_screen.dart';
+import 'package:scoped_model/scoped_model.dart';
 
 class LoginScreen extends StatelessWidget {
   final _formKey = GlobalKey<FormState>();
@@ -27,66 +29,73 @@ class LoginScreen extends StatelessWidget {
           ),
         ],
       ),
-      body: Form(
-        key: _formKey,
-        child: ListView(
-          padding: EdgeInsets.all(16.0),
-          children: <Widget>[
-            TextFormField(
-              decoration: InputDecoration(
-                hintText: "E-mail",
-              ),
-              keyboardType: TextInputType.emailAddress,
-              validator: (text) {
-                if (text.isEmpty || !text.contains("@"))
-                  return "E-mail inválido!";
-              },
-            ),
-            SizedBox(
-              height: 16.0,
-            ),
-            TextFormField(
-              decoration: InputDecoration(
-                hintText: "Senha",
-              ),
-              obscureText: true,
-              validator: (text) {
-                if (text.isEmpty || text.length < 6) return "Senha inválida!";
-              },
-            ),
-            Align(
-              alignment: Alignment.centerRight,
-              child: FlatButton(
-                padding: EdgeInsets.zero,
-                onPressed: () {},
-                child: Text(
-                  "Esqueci minha senha",
-                  textAlign: TextAlign.right,
+      body: ScopedModelDescendant<UserModel>(
+        builder: (context, child, model){
+          if (model.isLoading){
+            return Center(child: CircularProgressIndicator(),);
+          }
+          return Form(
+            key: _formKey,
+            child: ListView(
+              padding: EdgeInsets.all(16.0),
+              children: <Widget>[
+                TextFormField(
+                  decoration: InputDecoration(
+                    hintText: "E-mail",
+                  ),
+                  keyboardType: TextInputType.emailAddress,
+                  validator: (text) {
+                    if (text.isEmpty || !text.contains("@"))
+                      return "E-mail inválido!";
+                  },
                 ),
-              ),
-            ),
-            SizedBox(
-              height: 16.0,
-            ),
-            SizedBox(
-              height: 44.0,
-              child: RaisedButton(
-                textColor: Colors.white,
-                color: Theme.of(context).primaryColor,
-                child: Text(
-                  "Entrar",
-                  style: TextStyle(
-                    fontSize: 18.0,
+                SizedBox(
+                  height: 16.0,
+                ),
+                TextFormField(
+                  decoration: InputDecoration(
+                    hintText: "Senha",
+                  ),
+                  obscureText: true,
+                  validator: (text) {
+                    if (text.isEmpty || text.length < 6) return "Senha inválida!";
+                  },
+                ),
+                Align(
+                  alignment: Alignment.centerRight,
+                  child: FlatButton(
+                    padding: EdgeInsets.zero,
+                    onPressed: () {},
+                    child: Text(
+                      "Esqueci minha senha",
+                      textAlign: TextAlign.right,
+                    ),
                   ),
                 ),
-                onPressed: () {
-                  if (_formKey.currentState.validate()) {}
-                },
-              ),
+                SizedBox(
+                  height: 16.0,
+                ),
+                SizedBox(
+                  height: 44.0,
+                  child: RaisedButton(
+                    textColor: Colors.white,
+                    color: Theme.of(context).primaryColor,
+                    child: Text(
+                      "Entrar",
+                      style: TextStyle(
+                        fontSize: 18.0,
+                      ),
+                    ),
+                    onPressed: () {
+                      if (_formKey.currentState.validate()) {}
+                    },
+                  ),
+                ),
+              ],
             ),
-          ],
-        ),
-      ),
+          );
+        },
+      )
     );
   }
 }
